c754a8fe3cfa6da80322edf0c0207d472cbd3eb6
Pitman <michael.pitman@filament.uk.com>
2020-02-01 22:27:18 +0000

    Fix stuff

--- Assets/Scripts/Grounding.cs
+++ Assets/Scripts/Grounding.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
 public class Grounding : MonoBehaviour
 {
     public string groundTag;
---
+++ Assets/Scripts/WallCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WallCollision : MonoBehaviour
+{
+    public string groundTag;
+    public string wallTag;
+    public string personTag;
+    private int playerNumber;
+    private SoundManager soundManager;
+
+    void Start() {
+        soundManager = Camera.main.GetComponent<SoundManager>();
+        playerNumber = GetComponent<PlayerNumber>().playerNumber;
+    }
+
+    void OnCollisionEnter2D(Collider2D other) {
+    if (other.tag == groundTag || other.tag == wallTag || other.tag == personTag){
+            soundManager.OnImpact(playerNumber);
+        }
+    }
+}
