015aa809f6b124613e5c2d1d1eb13f1b55cba6f4
fierydrake <m.j.tunnicliffe@gmail.com>
2020-02-01 18:36:31 +0000

    Exit sound

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -11,11 +11,16 @@ public class Win : MonoBehaviour {
 
     public Text winText;
 
+    private SoundManager sound;
+
     void Start() {
         winText.gameObject.SetActive(false);
+        sound = Camera.main.GetComponent<SoundManager>();
     }
+
     void OnTriggerEnter2D(Collider2D col) {
         if ( col.gameObject == player1.gameObject || col.gameObject == player2.gameObject ) {
+            sound.OnExitLevel();
             winText.gameObject.SetActive(true);
             Destroy(player1.gameObject);
             Destroy(player2.gameObject);
