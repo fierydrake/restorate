02fdc6f2bc92e174a35f7862fa8672f99ede4b10
Pitman <michael.pitman@filament.uk.com>
2020-02-02 13:26:55 +0000

    Added player collision script

---
+++ Assets/Scripts/playerCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    public string personTag;
+    public int magnitude;
+    private int playerNumber;
+    public Rigidbody2D rb;
+    // private SoundManager soundManager;
+
+    void Start() {
+        // soundManager = Camera.main.GetComponent<SoundManager>();
+        // playerNumber = GetComponent<PlayerNumber>().playerNumber;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+    if (collision.gameObject.tag == personTag){
+            // soundManager.OnImpact(playerNumber);
+ 
+            var force = transform.position - collision.transform.position;
+ 
+            force.Normalize();
+            rb.AddForce(-force * magnitude);
+        }
+    }
+}
